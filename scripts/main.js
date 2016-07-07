c7a34608e2b6ad010f80903f4e0fed901cbf2328
Chad Estioco <chadestioco@gmail.com>
2016-07-07 18:19:42 +0800

    Haha unadded.

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+function(app){
+    document.addEventListener("DOMContentLoaded", function(){
+        ng.PlatformBrowserDynamic.bootstrap(app.AppComponent);
+    });
+}(window.app || (window.app = {}));
